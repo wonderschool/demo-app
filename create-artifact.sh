9a5f21acd3f8726db6921fbcc8d00a520b521c6d
Laszlo Fogas <laszlo@laszlo.cloud>
2021-08-25 13:56:55 +0200

    Triggering a deploy

--- create-artifact.sh
+++ create-artifact.sh
@@ -5,7 +5,7 @@ rm artifact.json
 echo "Creating artifact.."
 gimlet artifact create \
 --repository "laszlocph/gimletd-test-repo" \
---sha "b4f08b375865e0c484c0c98282e61860df99d129" \
+--sha "0fda000842458a21b575c3ac9a83fcea79ac04bb" \
 --created "2021-08-12T14:09:31+02:00" \
 --branch "main" \
 --event "push" \
@@ -14,7 +14,7 @@ gimlet artifact create \
 --committerName "Laszlo Fogas" \
 --committerEmail "laszlo@laszlo.cloud" \
 --message "Triggering a deploy" \
---url "https://github.com/laszlocph/gimletd-test-repo/commit/b4f08b375865e0c484c0c98282e61860df99d129" \
+--url "https://github.com/laszlocph/gimletd-test-repo/commit/0fda000842458a21b575c3ac9a83fcea79ac04bb" \
 > artifact.json
 
 echo "Attaching Gimlet manifests.."
@@ -24,5 +24,7 @@ do
     gimlet artifact add -f artifact.json --envFile $file
     fi
 done
+
+gimlet artifact add -f artifact.json --var SHA=0fda000842458a21b575c3ac9a83fcea79ac04bb
 
 # gimlet artifact push -f artifact.json
