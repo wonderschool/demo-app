a84b7e2cf13ee8dfacb8dbc74811b1ddef6e9985
Laszlo Fogas <laszlo@laszlo.cloud>
2021-09-09 17:07:41 +0200

    Scripting artifact creation

--- create-artifact.sh
+++ create-artifact.sh
@@ -1,20 +1,24 @@
 #!/usr/bin/env bash
 
+HASH=$(git log -1 --format=%H)
+DATE=$(git log -1 --format=%cI)
+MESSAGE=$(git log -1 --format=%s)
+
 rm artifact.json
 
 echo "Creating artifact.."
 gimlet artifact create \
 --repository "laszlocph/gimletd-test-repo" \
---sha "0fda000842458a21b575c3ac9a83fcea79ac04bb" \
---created "2021-08-12T14:09:31+02:00" \
+--sha "$HASH" \
+--created "$DATE" \
 --branch "main" \
 --event "push" \
 --authorName "Laszlo Fogas" \
 --authorEmail "laszlo@laszlo.cloud" \
 --committerName "Laszlo Fogas" \
 --committerEmail "laszlo@laszlo.cloud" \
---message "Triggering a deploy" \
---url "https://github.com/laszlocph/gimletd-test-repo/commit/0fda000842458a21b575c3ac9a83fcea79ac04bb" \
+--message "$MESSAGE" \
+--url "https://github.com/laszlocph/gimletd-test-repo/commit/$HASH" \
 > artifact.json
 
 echo "Attaching Gimlet manifests.."
@@ -25,6 +29,6 @@ do
     fi
 done
 
-gimlet artifact add -f artifact.json --var SHA=0fda000842458a21b575c3ac9a83fcea79ac04bb
+gimlet artifact add -f artifact.json --var SHA=$HASH
 
 # gimlet artifact push -f artifact.json
